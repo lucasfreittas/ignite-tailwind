81620452eb9de3a0935712e28b17353796e2cd8c
Lucas Freitas <l.freittas@outlook.com>
2025-11-19 19:47:37 -0300

    Initial Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
 
-export default eslintConfig;
+  // Ignorar pastas padrão
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+
+  // Habilitar Prettier
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+
+  // Deve ser sempre o último
+  prettier,
+]);
