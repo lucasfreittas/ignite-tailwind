d782b60f8f120f24865aa00e15208ae048eec4d4
Lucas Freitas <l.freittas@outlook.com>
2025-11-26 19:33:14 -0300

    Form start - Title + Label + button

--- src/components/ConfigurationTabs/TabUserDetails.tsx
+++ src/components/ConfigurationTabs/TabUserDetails.tsx
@@ -1,7 +1,30 @@
 export function TabUserDetails() {
   return (
-    <div>
-      <h1>Ola</h1>
-    </div>
+    <form className="h-full w-full" id="personalInfo">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-1">
+          <h2 className="text-lg font-medium text-zinc-900">Personal Info</h2>
+          <p className="text-sm font-normal text-zinc-500">
+            Update your photo and personal details here.
+          </p>
+        </div>
+        <div className="flex h-10 flex-row gap-3 text-sm font-semibold">
+          <button
+            type="button"
+            form="personalInfo"
+            className="w-20 cursor-pointer rounded-lg border border-zinc-300 px-4 py-2.5 text-zinc-700 hover:bg-zinc-50"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            form="personalInfo"
+            className="w-16 cursor-pointer rounded-lg border bg-violet-600 px-4 py-2.5 text-white hover:brightness-110"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
